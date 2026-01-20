0026874956bb06e883cd1315d7f68262f458030a
ELVIG17 <elvig.shakirov@gmail.com>
2026-01-20 14:27:25 +0300

    Update App.jsx

--- ascold/src/App.jsx
+++ ascold/src/App.jsx
@@ -1,16 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Header } from "./components/Header";
+import { Elementary } from "./components/Elementary";
+import { Discription } from "./components/Discription";
+import { ResultOfSchool } from "./components/ResultsOfSchool";
+import { Treners } from "./components/Treners";
 
 function App() {
-  Д
-
   return (
-    <>
-      123
-    </>
-  )
+    <div>
+      <Header />
+      <Elementary />
+      <Discription />
+      <ResultOfSchool />
+      <Treners />
+    </div>
+  );
 }
-
-export default App
+export default App;
