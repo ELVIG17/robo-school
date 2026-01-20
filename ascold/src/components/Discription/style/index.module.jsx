25a8d26739a06c3a1d5a9a423c4559b51a8399d9
ELVIG17 <elvig.shakirov@gmail.com>
2026-01-20 14:23:36 +0300

    Create index.module.jsx

---
+++ ascold/src/components/Discription/style/index.module.jsx
@@ -0,0 +1,30 @@
+.discription {
+  display: flex;
+  flex-direction: column;
+  width: 100vw;
+  background-color: #ffffff;
+  padding-top: 120px;
+}
+.newDiscription {
+  margin-left: 380px;
+  font-size: 30px;
+  max-width: 891px;
+}
+
+.stats {
+  display: flex;
+  flex-direction: row;
+  gap: 50px;
+  margin-left: 380px;
+}
+
+.numbers {
+  color: #d52027;
+  font-size: 70px;
+  padding: 16px 8px;
+}
+.discriptionForNumbers {
+  font-size: 18px;
+  font-weight: 400;
+  max-width: 260px;
+}
