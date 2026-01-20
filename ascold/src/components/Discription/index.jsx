0a138e0e4a5ea82281ea36f956aad6ce92c03b2b
ELVIG17 <elvig.shakirov@gmail.com>
2026-01-20 14:24:13 +0300

    Create index.jsx

---
+++ ascold/src/components/Discription/index.jsx
@@ -0,0 +1,38 @@
+import style from "./style/index.module.css";
+
+export const Discription = () => {
+  return (
+    <div className={style.discription}>
+      <p className={style.newDiscription}>
+        Robo School - учреждение для формирования кадрового педагогического
+        резерва в сфере робототехники и программирования
+      </p>
+      <div className={style.stats}>
+        <div>
+          <h1 className={style.numbers}>10</h1>
+          <p className={style.discriptionForNumbers}>
+            УМК по различным направлениям по робототехнике
+          </p>
+        </div>
+        <div>
+          <h1 className={style.numbers}>20</h1>
+          <p className={style.discriptionForNumbers}>
+            Школ, в которых запущена робототехника
+          </p>
+        </div>
+        <div>
+          <h1 className={style.numbers}>100</h1>
+          <p className={style.discriptionForNumbers}>
+            Педагогов прошедших курсы повышения квалификации
+          </p>
+        </div>
+        <div>
+          <h1 className={style.numbers}>10 000</h1>
+          <p className={style.discriptionForNumbers}>
+            Обученных детей на базе собственных центров
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
