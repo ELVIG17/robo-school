1a33949f3002bd245b582be377b3f3937158841d
ELVIG17 <elvig.shakirov@gmail.com>
2026-01-21 17:25:30 +0300

    Update index.jsx HEAQD

--- ascold/src/components/Header/index.jsx
+++ ascold/src/components/Header/index.jsx
@@ -4,13 +4,16 @@ import logo from "../../assets/logo.png";
 export const Header = () => {
   return (
     <header className={style.header}>
-      <h1 className={style.bigNameLeftHeader}>ROBO.SCHOOL</h1>
-      <div className={style.menuInHeader}>
-        <p>О школе</p>
-        <p>Тренеры</p>
-        <p>Стоимость</p>
+      <div className={style.contentHeader}>
+        <h1 className={style.bigNameLeftHeader}>ROBO.SCHOOL</h1>
+        <div className={style.menuInHeader}>
+          <p>О школе</p>
+          <p>Тренеры</p>
+          <p>Стоимость</p>
+        </div>
+        <h1 className={style.numberInHeader}>+7 800 000 11 22</h1>
       </div>
-      <h1 className={style.numberInHeader}>+7 800 000 11 22</h1>
     </header>
   );
 };
+
