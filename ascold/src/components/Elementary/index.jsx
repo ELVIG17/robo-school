053e9e5c9155c51155761fabd41dab322aca49ae
= <=>
2026-01-23 13:56:13 +0300

    добавил картинку женщины

--- ascold/src/components/Elementary/index.jsx
+++ ascold/src/components/Elementary/index.jsx
@@ -1,15 +1,19 @@
 import style from "./style/index.module.css";
+import woman from '../../assets/woman.svg'
 
 export const Elementary = () => {
   return (
     <div className={style.elementary}>
       <div className={style.contentElementary}>
-        <h1 className={style.mainNameInElementary}>ROBO SCHOOL</h1>
-        <p className={style.smallDescription}>
+        <div className={style.leftPart}>
+          <h1 className={style.mainNameInElementary}>ROBO SCHOOL</h1>
+          <p className={style.smallDescription}>
           Курсы повышения квалификации по робототехнике для педагогов начальной
           школы
-        </p>
-        <button className={style.buttomInElementary}>Записаться на курс</button>
+          </p>
+          <button className={style.buttomInElementary}>Записаться на курс</button>
+        </div>
+        <img className={style.womanImg} src={woman} alt="Woman"></img>
       </div>
     </div>
   );
