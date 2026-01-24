bca9c472d1538fe50411e784c121dcd367acbbea
ELVIG17 <elvig.shakirov@gmail.com>
2026-01-24 19:03:07 +0300

    Update App.jsx

--- ascold/src/App.jsx
+++ ascold/src/App.jsx
@@ -1,23 +1,9 @@
-import { Header } from "./components/Header";
-import { Elementary } from "./components/Elementary";
-import { Discription } from "./components/Discription";
-import { ResultOfSchool } from "./components/ResultsOfSchool";
-import { Treners } from "./components/Treners";
-import { ChoiceOfPocket } from "./components/ChoiceOfPocket";
-import { Anket } from "./components/Anket";
-import { Footer } from "./components/Footer";
+import { HomePage } from "./pages/HomePage/HomePage";
 
 function App() {
   return (
     <div>
-      <Header />
-      <Elementary />
-      <Discription />
-      <ResultOfSchool />
-      <Treners />
-      <ChoiceOfPocket />
-      <Anket />
-      <Footer />
+      <HomePage />
     </div>
   );
 }
