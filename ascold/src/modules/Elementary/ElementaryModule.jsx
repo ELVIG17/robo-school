5a8caf2dfe3491aafda86ce95e2c826b160c65a2
ELVIG17 <elvig.shakirov@gmail.com>
2026-01-24 19:13:57 +0300

    Create ElementaryModule.jsx

---
+++ ascold/src/modules/Elementary/ElementaryModule.jsx
@@ -0,0 +1,16 @@
+import style from "./style/index.module.css";
+
+export const Elementary = () => {
+  return (
+    <elementary className={style.elementary}>
+      <div className={style.contentElementary}>
+        <h1 className={style.mainNameInElementary}>ROBO SCHOOL</h1>
+        <p className={style.smallDescription}>
+          Курсы повышения квалификации по робототехнике для педагогов начальной
+          школы
+        </p>
+        <button className={style.buttomInElementary}>Записаться на курс</button>
+      </div>
+    </elementary>
+  );
+};
