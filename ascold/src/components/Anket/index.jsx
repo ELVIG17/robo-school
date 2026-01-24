cdff1844e338cf790e9ef7b6361d3f538ae1bfcc
ELVIG17 <elvig.shakirov@gmail.com>
2026-01-24 12:45:23 +0300

    Update index.jsx in anket

--- ascold/src/components/Anket/index.jsx
+++ ascold/src/components/Anket/index.jsx
@@ -5,25 +5,25 @@ export const Anket = () => {
     <div className={style.anket}>
       <div className={style.contentAnket}>
         <div className={style.leftBlock}>
-          <h1 className={style.sale}>Запишитесь на курс  со скидкой 10%</h1>
+          <h1 className={style.sale}>Запишитесь на курс со скидкой 10%</h1>
           <p className={style.promotion}>
             Акция действительна до 10 марта 2022 года
           </p>
         </div>
         <form className={style.form}>
           <div className={style.field}>
-            <label>Имя</label>
-            <input type="text" name="name" required></input>
+            <input type="text" placeholder="Name" name="name" required />
           </div>
           <div className={style.field}>
-            <label>Телефон</label>
-            <input type="number" name="Телефон" required></input>
+            <input type="tel" placeholder="Number" name="Phone" required />
           </div>
           <div className={style.field}>
-            <label>Email</label>
-            <input type="email" name="email" required></input>
+            <input type="email" placeholder="Email" name="email" required />
           </div>
-          <button type="submit"> Оставить заявку</button>
+          <button className={style.submitButton} type="submit">
+            {" "}
+            Оставить заявку
+          </button>
         </form>
       </div>
     </div>
