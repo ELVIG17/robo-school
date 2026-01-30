d6fd124c19719fc1a7973186653a274c8b50c0b5
ELVIG17 <elvig.shakirov@gmail.com>
2026-01-30 15:25:08 +0300

    badabing

--- ascold/src/modules/ElementaryModule/ElementaryModule.jsx
+++ ascold/src/modules/ElementaryModule/ElementaryModule.jsx
@@ -1,19 +1,18 @@
 import style from "./style/index.module.css";
 import woman from "../../assets/woman.svg"
 
+
 export const Elementary = () => {
   return (
-    <elementary className={style.elementary}>
+    <div className={style.elementary}>
       <div className={style.contentElementary}>
         <div className={style.leftPart}>
           <h1 className={style.mainNameInElementary}>ROBO SCHOOL</h1>
-          <p className={style.smallDescription}>
-          Курсы повышения квалификации по робототехнике для педагогов начальной
-          школы</p>
+          <p className={style.smallDescription}>Курсы повышения квалификации по робототехнике для педагогов начальнойшколы</p>
           <button className={style.buttomInElementary}>Записаться на курс</button>
         </div>
-        <img src={woman}></img>
+        <img src={woman} className={style.img}></img>
       </div>
-    </elementary>
+    </div>
   );
 };
--- ascold/src/pages/HomePage/HomePage.jsx
+++ ascold/src/pages/HomePage/HomePage.jsx
@@ -8,15 +8,15 @@ import { Footer } from "../../modules/FooterModule";
 import { Anket } from "../../modules/AnketModule";
 
 export const HomePage = () => {
-  console.log("ПРОВЕРКА:");
-  console.log("Header:", Header);
-  console.log("Elementary:", Elementary);
-  console.log("Description:", Description);
-  console.log("ResultOfSchool:", ResultOfSchool);
-  console.log("Treners:", Treners);
-  console.log("ChoiceOfPocket:", ChoiceOfPocket);
-  console.log("Anket:", Anket);
-  console.log("Footer:", Footer);
+  // console.log("ПРОВЕРКА:");
+  // console.log("Header:", Header);
+  // console.log("Elementary:", Elementary);
+  // console.log("Description:", Description);
+  // console.log("ResultOfSchool:", ResultOfSchool);
+  // console.log("Treners:", Treners);
+  // console.log("ChoiceOfPocket:", ChoiceOfPocket);
+  // console.log("Anket:", Anket);
+  // console.log("Footer:", Footer);
   return (
     <>
       <Header />
