e97a3c6c096d35cb12edf07cb3adb3abe5f072e4
= <=>
2026-01-30 13:57:51 +0300

    make woman

--- ascold/src/modules/ElementaryModule/ElementaryModule.jsx
+++ ascold/src/modules/ElementaryModule/ElementaryModule.jsx
@@ -1,15 +1,18 @@
 import style from "./style/index.module.css";
+import woman from "../../assets/woman.svg"
 
 export const Elementary = () => {
   return (
     <elementary className={style.elementary}>
       <div className={style.contentElementary}>
-        <h1 className={style.mainNameInElementary}>ROBO SCHOOL</h1>
-        <p className={style.smallDescription}>
+        <div className={style.leftPart}>
+          <h1 className={style.mainNameInElementary}>ROBO SCHOOL</h1>
+          <p className={style.smallDescription}>
           Курсы повышения квалификации по робототехнике для педагогов начальной
-          школы
-        </p>
-        <button className={style.buttomInElementary}>Записаться на курс</button>
+          школы</p>
+          <button className={style.buttomInElementary}>Записаться на курс</button>
+        </div>
+        <img src={woman}></img>
       </div>
     </elementary>
   );
